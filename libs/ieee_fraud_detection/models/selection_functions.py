69381cefe8da4936c0631e501ebefd347c132aee
Nick Desmond <nick.desmond@nudatasecurity.com>
2020-01-26 16:45:36 -0800

    Separated selection functions and added time-based features

---
+++ libs/ieee_fraud_detection/models/selection_functions.py
@@ -0,0 +1,248 @@
+import numpy as np
+
+def col_TransactionAmt(X):
+    return np.log10(X[['TransactionAmt']])
+
+
+def col_V1(X):
+    return X[['V1']].fillna('NaN')
+
+
+def col_V1NaN(X):
+    return X[['V1']] == 'NaN'
+
+
+def col_V2(X):
+    return X[['V2']].fillna('NaN')
+
+
+def col_V2NaN(X):
+    return X[['V2']] == 'NaN'
+
+
+def col_V3(X):
+    return X[['V3']].fillna('NaN')
+
+
+def col_V3NaN(X):
+    return X[['V3']] == 'NaN'
+
+
+def col_V4(X):
+    return X[['V4']].fillna('NaN')
+
+
+def col_V4NaN(X):
+    return X[['V4']] == 'NaN'
+
+
+def col_V5(X):
+    return X[['V5']].fillna('NaN')
+
+
+def col_V5NaN(X):
+    return X[['V5']] == 'NaN'
+
+
+def col_V6(X):
+    return X[['V6']].fillna('NaN')
+
+
+def col_V6NaN(X):
+    return X[['V6']] == 'NaN'
+
+
+def col_V7(X):
+    return X[['V7']].fillna('NaN')
+
+
+def col_V7NaN(X):
+    return X[['V7']] == 'NaN'
+
+
+def col_V8(X):
+    return X[['V8']].fillna('NaN')
+
+
+def col_V8NaN(X):
+    return X[['V8']] == 'NaN'
+
+
+def col_V9(X):
+    return X[['V9']].fillna('NaN')
+
+
+def col_V9NaN(X):
+    return X[['V9']] == 'NaN'
+
+
+def col_V10(X):
+    return X[['V10']].fillna('NaN')
+
+
+def col_V10NaN(X):
+    return X[['V10']] == 'NaN'
+
+
+def col_V11(X):
+    return X[['V11']].fillna('NaN')
+
+
+def col_V11NaN(X):
+    return X[['V11']] == 'NaN'
+
+
+def col_V12(X):
+    return X[['V12']].fillna('NaN')
+
+
+def col_V12NaN(X):
+    return X[['V12']] == 'NaN'
+
+
+def col_V13(X):
+    return X[['V13']].fillna('NaN')
+
+
+def col_V13NaN(X):
+    return X[['V13']] == 'NaN'
+
+
+def col_V14(X):
+    return X[['V14']].fillna('NaN')
+
+
+def col_V14NaN(X):
+    return X[['V14']] == 'NaN'
+
+
+def col_V15(X):
+    return X[['V15']].fillna('NaN')
+
+
+def col_V15NaN(X):
+    return X[['V15']] == 'NaN'
+
+
+def col_V16(X):
+    return X[['V16']].fillna('NaN')
+
+
+def col_V16NaN(X):
+    return X[['V16']] == 'NaN'
+
+
+def col_V17(X):
+    return X[['V17']].fillna('NaN')
+
+
+def col_V17NaN(X):
+    return X[['V17']] == 'NaN'
+
+
+def col_V18(X):
+    return X[['V18']].fillna('NaN')
+
+
+def col_V18NaN(X):
+    return X[['V18']] == 'NaN'
+
+
+def col_V19(X):
+    return X[['V19']].fillna('NaN')
+
+
+def col_V19NaN(X):
+    return X[['V19']] == 'NaN'
+
+
+
+
+
+
+def col_dist1(X):
+    return np.log10(X[['dist1']].astype(float) + 1).fillna('NaN')
+
+
+def col_dist1NaN(X):
+    return X[['dist1']] == 'NaN'
+
+
+def col_dist2(X):
+    return X[['dist2']].fillna('NaN')
+
+
+def col_dist2NaN(X):
+    return X[['dist2']] == 'NaN'
+
+
+
+def col_ProductCD(X):
+    return X[['ProductCD']].fillna('NaN')
+
+
+def col_card4(X):
+    return X[['card4']].fillna('NaN')
+
+
+def col_card5(X):
+    return X[['card5']].fillna('NaN')
+
+
+def col_card6(X):
+    return X[['card6']].fillna('NaN')
+
+
+def col_P_emaildomain(X):
+    return X[['P_emaildomain']].fillna('NaN')
+
+
+def col_R_emaildomain(X):
+    return X[['R_emaildomain']].fillna('NaN')
+
+
+def col_M4(X):
+    return X[['M4']].fillna('NaN')
+
+
+
+
+def col_DeviceType(X):
+    return X[['DeviceType']].fillna('NaN')
+
+def col_DeviceInfo(X):
+    return X[['DeviceInfo']].fillna('NaN')
+
+def col_id_12(X):
+    return X[['id_12']].fillna('NaN')
+
+def col_id_15(X):
+    return X[['id_15']].fillna('NaN')
+
+def col_id_16(X):
+    return X[['id_16']].fillna('NaN')
+
+def col_id_23(X):
+    return X[['id_23']].fillna('NaN')
+
+def col_id_27(X):
+    return X[['id_27']].fillna('NaN')
+
+def col_id_28(X):
+    return X[['id_28']].fillna('NaN')
+
+def col_id_29(X):
+    return X[['id_29']].fillna('NaN')
+
+def col_id_34(X):
+    return X[['id_34']].fillna('NaN')
+
+
+def col_hourofday(X):
+    return X[['hourofday']]
+
+def col_dayofweek(X):
+    return X[['dayofweek']]
+
+def col_dayofmonth(X):
+    return X[['dayofmonth']]
--- libs/ieee_fraud_detection/models/train_classifier.py
+++ libs/ieee_fraud_detection/models/train_classifier.py
@@ -16,7 +16,10 @@ from sklearn.preprocessing import FunctionTransformer, LabelEncoder, OneHotEncod
 from sklearn.impute import SimpleImputer
 
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import LinearSVC
 
 from sklearn import metrics
@@ -25,6 +28,7 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, IterativeImputer
 
 from ieee_fraud_detection.data.process_data import save_data
+from ieee_fraud_detection.models.selection_functions import *
 
 def load_data(database_filepath):
     """
@@ -54,7 +58,30 @@ def preprocess_data(X, y):
     :return: Further preprocessed dataset
     """
     
-    fillna_cols = ['card4', 'card6', 'P_emaildomain', 'R_emaildomain', 'M4', 'V2', 'V3']
+    # Encode time based variables
+    X['mins'] = np.floor(X.TransactionDT / 60)
+    X['hours'] = np.floor(X['mins'] / 60)
+    X['days'] = np.floor(X['hours'] / 24)
+    X['weeks'] = np.floor(X['days'] / 7)
+    X['months'] = np.floor(X['days'] / 365 * 12)
+    X['years'] = np.floor(X['days'] / 365)
+    
+    X['dayofweek'] = np.mod(X['days'] - X['weeks'] * 7, 7)
+    X['dayofmonth'] = np.floor(np.mod(X['days'] - np.floor(X['months'] * 365 / 12), 365 / 12))
+    
+    X['hourofday'] = np.mod(X['hours'] - X['days'] * 24, 24)
+    
+    fillna_cols = ['card4', 'card5', 'card6',
+                   'P_emaildomain', 'R_emaildomain', 'M4',
+                   'dist1', 'dist2',
+                   'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11',
+                   'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21',
+                   'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'V29', 'V30', 'V31',
+                   'V32', 'V33', 'V34', 'V35', 'V36', 'V37', 'V38', 'V39', 'V40', 'V41',
+                   'V42', 'V43', 'V44', 'V45', 'V46',
+                   'DeviceType', 'DeviceInfo',
+                   'id_12', 'id_15', 'id_16', 'id_23', 'id_27', 'id_28', 'id_29', 'id_34'
+                   ]
     X[fillna_cols] = X[fillna_cols].fillna('NaN')
     
     return X, y
@@ -124,50 +151,6 @@ def import_data(database_filepath, cols, resample=True, resample_suffix='_rs'):
         return X, y
 
 
-def col_TransactionAmt(X):
-    return X[['TransactionAmt']]
-
-
-def col_V2(X):
-    return X[['V2']].fillna('NaN')
-
-
-def col_V2NaN(X):
-    return X[['V2']] == 'NaN'
-
-
-def col_V3(X):
-    return X[['V3']].fillna('NaN')
-
-
-def col_V3NaN(X):
-    return X[['V3']] == 'NaN'
-
-
-def col_ProductCD(X):
-    return X[['ProductCD']].fillna('NaN')
-
-
-def col_card4(X):
-    return X[['card4']].fillna('NaN')
-
-
-def col_card6(X):
-    return X[['card6']].fillna('NaN')
-
-
-def col_P_emaildomain(X):
-    return X[['P_emaildomain']].fillna('NaN')
-
-
-def col_R_emaildomain(X):
-    return X[['R_emaildomain']].fillna('NaN')
-
-
-def col_M4(X):
-    return X[['M4']].fillna('NaN')
-
-
 class ModifiedLabelEncoder(LabelEncoder):
 
     def fit_transform(self, y, *args, **kwargs):
@@ -191,6 +174,13 @@ def build_model():
     pipe_TransactionAmt = Pipeline([
         ('column_selection', FunctionTransformer(col_TransactionAmt, validate=False))
     ])
+    pipe_V1 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V1, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V1NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V1NaN, validate=False))
+    ])
     pipe_V2 = Pipeline([
         ('column_selection', FunctionTransformer(col_V2, validate=False)),
         ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
@@ -205,6 +195,134 @@ def build_model():
     pipe_V3NaN = Pipeline([
         ('column_selection', FunctionTransformer(col_V3NaN, validate=False))
     ])
+    pipe_V4 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V4, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V4NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V4NaN, validate=False))
+    ])
+    pipe_V5 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V5, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V5NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V5NaN, validate=False))
+    ])
+    pipe_V6 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V6, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V6NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V6NaN, validate=False))
+    ])
+    pipe_V7 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V7, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V7NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V7NaN, validate=False))
+    ])
+    pipe_V8 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V8, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V8NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V8NaN, validate=False))
+    ])
+    pipe_V9 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V9, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V9NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V9NaN, validate=False))
+    ])
+    pipe_V10 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V10, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V10NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V10NaN, validate=False))
+    ])
+    pipe_V11 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V11, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V11NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V11NaN, validate=False))
+    ])
+    pipe_V12 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V12, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V12NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V12NaN, validate=False))
+    ])
+    pipe_V13 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V13, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V13NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V13NaN, validate=False))
+    ])
+    pipe_V14 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V14, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V14NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V14NaN, validate=False))
+    ])
+    pipe_V15 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V15, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V15NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V15NaN, validate=False))
+    ])
+    pipe_V16 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V16, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V16NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V16NaN, validate=False))
+    ])
+    pipe_V17 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V17, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V17NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V17NaN, validate=False))
+    ])
+    pipe_V18 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V18, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V18NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V18NaN, validate=False))
+    ])
+    pipe_V19 = Pipeline([
+        ('column_selection', FunctionTransformer(col_V19, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='most_frequent'))
+    ])
+    pipe_V19NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_V19NaN, validate=False))
+    ])
+
+
+    pipe_dist1 = Pipeline([
+        ('column_selection', FunctionTransformer(col_dist1, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='constant', fill_value=0))
+    ])
+    pipe_dist1NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_dist1NaN, validate=False))
+    ])
+    pipe_dist2 = Pipeline([
+        ('column_selection', FunctionTransformer(col_dist2, validate=False)),
+        ('imputer', SimpleImputer(missing_values='NaN', strategy='constant', fill_value=0))
+    ])
+    pipe_dist2NaN = Pipeline([
+        ('column_selection', FunctionTransformer(col_dist2NaN, validate=False))
+    ])
     
     
     ## CATEGORICALS
@@ -214,6 +332,10 @@ def build_model():
     ])
     pipe_card4 = Pipeline([
         ('column_selection', FunctionTransformer(col_card4, validate=False)),
+        onehot
+    ])
+    pipe_card5 = Pipeline([
+        ('column_selection', FunctionTransformer(col_card5, validate=False)),
         onehot
     ])
     pipe_card6 = Pipeline([
@@ -232,28 +354,146 @@ def build_model():
         ('column_selection', FunctionTransformer(col_M4, validate=False)),
         onehot
     ])
+    pipe_DeviceType = Pipeline([
+        ('column_selection', FunctionTransformer(col_DeviceType, validate=False)),
+        onehot
+    ])
+    pipe_DeviceInfo = Pipeline([
+        ('column_selection', FunctionTransformer(col_DeviceInfo, validate=False)),
+        onehot
+    ])
+    pipe_id_12 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_12, validate=False)),
+        onehot
+    ])
+    pipe_id_15 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_15, validate=False)),
+        onehot
+    ])
+    pipe_id_16 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_16, validate=False)),
+        onehot
+    ])
+    pipe_id_23 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_23, validate=False)),
+        onehot
+    ])
+    pipe_id_27 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_27, validate=False)),
+        onehot
+    ])
+    pipe_id_28 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_28, validate=False)),
+        onehot
+    ])
+    pipe_id_29 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_29, validate=False)),
+        onehot
+    ])
+    pipe_id_34 = Pipeline([
+        ('column_selection', FunctionTransformer(col_id_34, validate=False)),
+        onehot
+    ])
+    pipe_hourofday = Pipeline([
+        ('column_selection', FunctionTransformer(col_hourofday, validate=False)),
+        onehot
+    ])
+    pipe_dayofweek = Pipeline([
+        ('column_selection', FunctionTransformer(col_dayofweek, validate=False)),
+        onehot
+    ])
+    pipe_dayofmonth = Pipeline([
+        ('column_selection', FunctionTransformer(col_dayofmonth, validate=False)),
+        onehot
+    ])
 
     pipe = Pipeline([
         ('union', FeatureUnion([
             ('pipe_TransactionAmt', pipe_TransactionAmt),
+            # ('pipe_V1', pipe_V1),
+            # ('pipe_V1NaN', pipe_V1NaN),
             ('pipe_V2', pipe_V2),
             ('pipe_V2NaN', pipe_V2NaN),
             ('pipe_V3', pipe_V3),
             ('pipe_V3NaN', pipe_V3NaN),
+            # ('pipe_V4', pipe_V4),
+            # ('pipe_V4NaN', pipe_V4NaN),
+            ('pipe_V5', pipe_V5),
+            # ('pipe_V5NaN', pipe_V5NaN),
+            # ('pipe_V6', pipe_V6),
+            # ('pipe_V6NaN', pipe_V6NaN),
+            # ('pipe_V7', pipe_V7),
+            # ('pipe_V7NaN', pipe_V7NaN),
+            # ('pipe_V8', pipe_V8),
+            # ('pipe_V8NaN', pipe_V8NaN),
+            # ('pipe_V9', pipe_V9),
+            # ('pipe_V9NaN', pipe_V9NaN),
+            # ('pipe_V10', pipe_V10),
+            # ('pipe_V10NaN', pipe_V10NaN),
+            # ('pipe_V11', pipe_V11),
+            # ('pipe_V11NaN', pipe_V11NaN),
+            # ('pipe_V12', pipe_V12),
+            # ('pipe_V12NaN', pipe_V12NaN),
+            # ('pipe_V13', pipe_V13),
+            # ('pipe_V13NaN', pipe_V13NaN),
+            # ('pipe_V14', pipe_V14),
+            # ('pipe_V14NaN', pipe_V14NaN),
+            # ('pipe_V15', pipe_V15),
+            # ('pipe_V15NaN', pipe_V15NaN),
+            # ('pipe_V16', pipe_V16),
+            # ('pipe_V16NaN', pipe_V16NaN),
+            # ('pipe_V17', pipe_V17),
+            # ('pipe_V17NaN', pipe_V17NaN),
+            # ('pipe_V18', pipe_V18),
+            # ('pipe_V18NaN', pipe_V18NaN),
+            # ('pipe_V19', pipe_V19),
+            # ('pipe_V19NaN', pipe_V19NaN),
+            ('pipe_dist1', pipe_dist1),
+            ('pipe_dist1NaN', pipe_dist1NaN),
+            ('pipe_dist2', pipe_dist2),
+            ('pipe_dist2NaN', pipe_dist2NaN),
+            
+            
             ('pipe_ProductCD', pipe_ProductCD),
             ('pipe_card4', pipe_card4),
+            # ('pipe_card5', pipe_card5),
             ('pipe_card6', pipe_card6),
             ('pipe_P_emaildomain', pipe_P_emaildomain),
             ('pipe_R_emaildomain', pipe_R_emaildomain),
-            ('pipe_M4', pipe_M4)
+            ('pipe_M4', pipe_M4),
+            ('pipe_DeviceType', pipe_DeviceType),
+            # ('pipe_DeviceInfo', pipe_DeviceInfo),
+            ('pipe_id_12', pipe_id_12),
+            ('pipe_id_15', pipe_id_15),
+            ('pipe_id_16', pipe_id_16),
+            ('pipe_id_23', pipe_id_23),
+            ('pipe_id_27', pipe_id_27),
+            ('pipe_id_28', pipe_id_28),
+            ('pipe_id_29', pipe_id_29),
+            # ('pipe_id_34', pipe_id_34),
+
+            ('pipe_hourofday', pipe_hourofday),
+            ('pipe_dayofweek', pipe_dayofweek),
+            ('pipe_dayofmonth', pipe_dayofmonth)
         ])),
+        # ('feature_selection', SelectFromModel(RandomForestClassifier(n_estimators=10))),
         ('clf', RandomForestClassifier())
     ])
 
     # Set up a parameter grid
     pg = [
         {
-            'clf': [RandomForestClassifier()]
+            'clf': [RandomForestClassifier()],
+            'clf__n_estimators': [10],
+            'clf__criterion': 'entropy', # ['gini', 'entropy'],
+            'clf__min_samples_split': [10], # [10,20,50,100],
+            'clf__min_samples_leaf': [1], # [1,2,5,10],
+            'clf__min_weight_fraction_leaf': [0], # [0, 0.01, 0.1, 0.5],
+            'clf__max_features': [None], # ['auto', 'sqrt', 'log2', None],
+            'clf__bootstrap': [True], # [True, False],
+            'clf__oob_score': [False], # [False, True],
+            # 'clf__random_state': [0],
+            # 'clf__ccp_alpha': [0.0, 0.05, 0.10, 0.5, 1.0]
         }
     ]
 
@@ -319,10 +559,25 @@ def main(database_filepath='../data/train_transactions.db',
         database_filepath, model_filepath = sys.argv[1:]
     
     print('Importing data...')
+
     X, y = import_data(
         database_filepath, 
-        ['TransactionAmt', 'V2', 'V3', 'ProductCD', 'card4', 'card6', 'P_emaildomain', 'R_emaildomain', 'M4']
+        ['TransactionAmt', 'ProductCD',
+           'card4', 'card5', 'card6',
+           'P_emaildomain', 'R_emaildomain', 'M4',
+           'dist1', 'dist2',
+           'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11',
+           'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21',
+           'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'V29', 'V30', 'V31',
+           'V32', 'V33', 'V34', 'V35', 'V36', 'V37', 'V38', 'V39', 'V40', 'V41',
+           'V42', 'V43', 'V44', 'V45', 'V46',
+           'DeviceType', 'DeviceInfo',
+           'id_12', 'id_15', 'id_16', 'id_23', 'id_27', 'id_28', 'id_29', 'id_34',
+           'weeks', 'months', 'hourofday', 'dayofweek', 'dayofmonth'
+        ]
     )
+
+    print(X.shape)
 
     print('Creating train test split from data...')
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
